a0c1084695ba98245056fe0a2e1228775a2e9751
McMurdo1 <matt@1423ventures.com>
2014-01-21 13:37:17 -0600

    Initial Commit

---
+++ GetOnThatBus/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GetOnThatBus
+//
+//  Created by Matthew Graham on 1/21/14.
+//  Copyright (c) 2014 Matthew Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
